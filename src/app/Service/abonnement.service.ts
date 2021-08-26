f89e82c949f3b2dc131e18e850775590e87f5db8
BenjaminMayot <benjamin.mayot@laposte.net>
2021-08-26 15:56:45 +0200

    abonnements

--- src/app/Service/abonnement.service.ts
+++ src/app/Service/abonnement.service.ts
@@ -5,15 +5,17 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ReservationService {
-  private baseURL = "http://localhost:9090/gestionEvenements/reservation";
+export class AbonnementService {
+
+  private baseURL = "http://localhost:9090/gestionAbonnements/abonnement";
   constructor(private httpClient: HttpClient) { }
+
   public findAll():Observable<any>{
     return this.httpClient.get(this.baseURL);
   }
  
   public delete(id:number):Observable<any>{
-    return this.httpClient.delete(this.baseURL+"/"+id); 
+    return this.httpClient.delete(this.baseURL+"/"+id); // permet de supprimer avec l'id dans la requete http : http://localhost:9090/gestionUsers/users/1
   }
 
   public save(user:any){
---
+++ src/app/models/abonnement.ts
@@ -0,0 +1,9 @@
+export class Abonnement {
+    idAbonnement:number=0;
+    nom:string="";
+    tarif:number=0;
+    dateDebut?:Date;
+    dateFin?:Date;
+    
+
+}
